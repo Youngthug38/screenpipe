bb7f3463725faf82c0119e351c26c0b994c2815d
github-action-benchmark <github@users.noreply.github.com>
2026-01-23 00:12:45 +0000

    add OCR Benchmarks (cargo) benchmark result for 44a1ba009630492476e28d61f35615a64c5281cb

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1769127165892,
+  "repoUrl": "https://github.com/Youngthug38/screenpipe",
+  "entries": {
+    "OCR Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "mrmousawi@hotmail.com",
+            "name": "Youngthug38",
+            "username": "Youngthug38"
+          },
+          "committer": {
+            "email": "mrmousawi@hotmail.com",
+            "name": "Youngthug38",
+            "username": "Youngthug38"
+          },
+          "distinct": true,
+          "id": "44a1ba009630492476e28d61f35615a64c5281cb",
+          "message": "fix: use built-in GITHUB_TOKEN",
+          "timestamp": "2026-01-23T01:01:10+01:00",
+          "tree_id": "97077e2e4553e1d20da5ece93de9f65c6f9f97c4",
+          "url": "https://github.com/Youngthug38/screenpipe/commit/44a1ba009630492476e28d61f35615a64c5281cb"
+        },
+        "date": 1769127164813,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Apple Vision OCR/Performance/",
+            "value": 3005697583,
+            "range": "± 415116392",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
